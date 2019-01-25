933ce4ca8c660cda56f6267b7c3077eed6e00f2d
azak <zak.aleksander@gmailcom>
2019-01-25 14:58:12 +0100

    increase number of controller instances to two.

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,9 +28,9 @@ module "network" {
 }
 
 module "controllers" {
-  source = "modules\/compute"
+  source = "modules/compute"
 
-  instances_count       = "1"
+  instances_count       = "${var.controllers_count}"
   username              = "zakal"
   ssh_key               = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC/7PmTvGgwtX4fiZGyKXlULsPzhUM2j3KQSqg1HlAp9TmDyTVvC87NQNbP28DqJPwm+w/BR0Fghdm4O8YC5evQdExMWg6oPbdp9FdRi1w+hCyArBaDVd//+m9BzNYjyx+NHMB75wBdcY0QsC5chD/qS/R6uEk6eE/31oAbBOAJLCAPqwpI50E1ueDoVYlrtVCEQLLWMRMy4eaSbf4lg8K5xOiDpMzpMCTn2YjK16EjedrQJQt6oQmQt8QfCkHfYqGp2FSuRi3/sb+8xURvNdnOvLm8nb+a6I1XnCfNvp9NBeUDZXaH+TwKPfC7gAaBEmymmmNPuRwMcEG962+ku4ul"
   location              = "${azurerm_resource_group.kthw.location}"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,3 +5,7 @@ variable "location" {
 variable "prefix" {
   default = "kthw"
 }
+
+variable "controllers_count" {
+  default = "2"
+}
